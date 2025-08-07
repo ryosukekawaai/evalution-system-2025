51b4242db94fb55355c18dd782a145f5356575f3
ryosuke.kawaai <ryosuke.kawaai@insight-inc.biz>
2025-08-07 20:07:53 +0900

    更新

--- src/exportToSheets/createFolder.js
+++ src/exportToSheets/createFolder.js
@@ -1,13 +1,38 @@
 const { google } = require('googleapis');
 const { auth } = require('./utils');
 
+const drive = google.drive({ version: 'v3', auth });
+
+// フォルダの存在確認
+const folderExists = async (folderId) => {
+    try {
+        await drive.files.get({
+            fileId: folderId,
+            fields: 'id',
+        });
+        return true;
+    } catch (error) {
+        console.warn(`⚠️ 指定されたフォルダID(${folderId})は存在しないかアクセスできません`);
+        return false;
+    }
+};
+
 const createFolder = async (name, parentId = null) => {
-    const drive = google.drive({ version: 'v3', auth });
+    let parents = [];
+
+    if (parentId) {
+        const exists = await folderExists(parentId);
+        if (exists) {
+            parents.push(parentId);
+        } else {
+            console.warn(`⛔ 親フォルダが存在しないため、親なしで ${name} を作成します`);
+        }
+    }
 
     const fileMetadata = {
         name,
         mimeType: 'application/vnd.google-apps.folder',
-        ...(parentId && { parents: [parentId] })
+        ...(parents.length > 0 ? { parents } : {})
     };
 
     const res = await drive.files.create({
